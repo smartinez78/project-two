32541f238af793ba12e9d32f4b37e78360d893a9
Sofia Martinez <smartinez@hawk.iit.edu>
2018-12-06 22:49:58 -0600

    Add function to check browser quality, and if/else statement to modify value of display property

--- site.js
+++ site.js
@@ -1,5 +1,15 @@
 "use strict";
 
-(function(){
-
-})(); // end of function()
+//immediately invoked function
+(function buttonToggle(){
+  // check browser quality
+  if (typeof(document.querySelector)==="undefined") {
+    return;
+  }
+      if (content.style.display == "block") {
+          content.style.display = "none";
+      }
+      else {
+          content.style.display = "block";
+      }
+})();
