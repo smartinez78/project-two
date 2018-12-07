13df11667be86844e3cf18b3d83484515420a878
Sofia Martinez <smartinez@hawk.iit.edu>
2018-12-06 23:28:41 -0600

    Add comments to explain javascript components

--- site.js
+++ site.js
@@ -2,6 +2,7 @@
 
 //immediately invoked function
 (function buttonToggle(){
+  //create 'array' of elements that have "project-button" class tag
   var totButtons = document.getElementsByClassName("project-button");
   var num;
   // check browser quality
@@ -9,14 +10,17 @@
     return;
   }
   for (num = 0; num < totButtons.length; num++) {
+    //event listener on elements in array (project buttons)
     totButtons[num].addEventListener("click", function modifyDisplay() {
       var content = this.nextElementSibling;
+      //upon click, if display is not equal to block, set to block (show content)
       if (content.style.display !== "block") {
           content.style.display = "block";
       }
+      //upon click, if display is equal to block, set to none (hide content)
       else {
           content.style.display = "none";
       }
     });
   }
-})();
+})();//end of function
