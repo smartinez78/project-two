6a265e6b8f562489dbbbb9d58dbbc1eb411e9c51
Sofia Martinez <smartinez@hawk.iit.edu>
2018-12-06 23:17:08 -0600

    Add for loop and variables to account for both buttons in "project-button" class

--- site.js
+++ site.js
@@ -2,14 +2,21 @@
 
 //immediately invoked function
 (function buttonToggle(){
+  var totButtons = document.getElementsByClassName("project-button");
+  var num;
   // check browser quality
   if (typeof(document.querySelector)==="undefined") {
     return;
   }
-      if (content.style.display == "block") {
-          content.style.display = "none";
-      }
-      else {
+  for (num = 0; num < totButtons.length; num++) {
+    totButtons[num].addEventListener("click", function modifyDisplay() {
+      var content = this.nextElementSibling;
+      if (content.style.display !== "block") {
           content.style.display = "block";
       }
+      else {
+          content.style.display = "none";
+      }
+    });
+  }
 })();
